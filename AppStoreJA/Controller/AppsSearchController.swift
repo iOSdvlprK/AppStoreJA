c93f840e6ec31584fdac248c6f0675270ef7651b
iOS developer K <iOSdvlprK@gmail.com>
2023-04-24 22:26:45 +0900

    cells and UIStackView layouts in Search

--- AppStoreJA/Controller/AppsSearchController.swift
+++ AppStoreJA/Controller/AppsSearchController.swift
@@ -7,12 +7,29 @@
 
 import UIKit
 
-class AppsSearchController: UICollectionViewController {
+class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    fileprivate let cellId = "id1234"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .systemRed
+        collectionView.backgroundColor = .systemBackground
+        
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 250)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
     }
     
     init() {

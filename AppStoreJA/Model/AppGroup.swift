f7ecbcd217aec92df256658cca18cf7927fda642
iOS developer K <iOSdvlprK@gmail.com>
2023-04-30 22:12:02 +0900

    fetch AppGroup JSON data

---
+++ AppStoreJA/Model/AppGroup.swift
@@ -0,0 +1,21 @@
+//
+//  AppGroup.swift
+//  AppStoreJA
+//
+//  Created by joe on 2023/04/30.
+//
+
+import Foundation
+
+struct AppGroup: Decodable {
+    let feed: Feed
+}
+
+struct Feed: Decodable {
+    let title: String
+    let results: [FeedResult]
+}
+
+struct FeedResult: Decodable {
+    let name, artistName, artworkUrl100: String
+}

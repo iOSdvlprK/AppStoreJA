b731bfe42616c1495b6e3f1d9dc770702892c656
iOS developer K <iOSdvlprK@gmail.com>
2023-05-05 22:56:05 +0900

    let AppsPageController handle user selection in AppsHorizontalController using closure

---
+++ AppStoreJA/Controller/Apps/Details/AppDetailController.swift
@@ -0,0 +1,16 @@
+//
+//  AppDetailController.swift
+//  AppStoreJA
+//
+//  Created by joe on 2023/05/05.
+//
+
+import UIKit
+
+class AppDetailController: BaseListController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .systemYellow
+    }
+}

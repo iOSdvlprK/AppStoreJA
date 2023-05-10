17a66ebbea4790d3df07b3664f032bb510951f02
iOS developer K <iOSdvlprK@gmail.com>
2023-05-10 21:39:28 +0900

    lay out app reviews controller

---
+++ AppStoreJA/Views/Apps/Details/ReviewRowCell.swift
@@ -0,0 +1,26 @@
+//
+//  ReviewRowCell.swift
+//  AppStoreJA
+//
+//  Created by joe on 2023/05/10.
+//
+
+import UIKit
+
+class ReviewRowCell: UICollectionViewCell {
+    
+    let reviewsController = ReviewsController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemYellow
+        
+        addSubview(reviewsController.view)
+        reviewsController.view.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

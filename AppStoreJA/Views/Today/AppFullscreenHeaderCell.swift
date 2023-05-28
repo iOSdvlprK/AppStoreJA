3165b5742119029b043831742b5f093b442d0cf5
iOS developer K <iOSdvlprK@gmail.com>
2023-05-28 22:43:35 +0900

    floating controls view

--- AppStoreJA/Views/Today/AppFullscreenHeaderCell.swift
+++ AppStoreJA/Views/Today/AppFullscreenHeaderCell.swift
@@ -16,9 +16,6 @@ class AppFullscreenHeaderCell: UITableViewCell {
         
         contentView.addSubview(todayCell)
         todayCell.fillSuperview()
-        
-//        contentView.addSubview(closeButton)
-//        closeButton.anchor(top: topAnchor, leading: nil, bottom: nil, trailing: trailingAnchor, padding: UIEdgeInsets(top: 44, left: 0, bottom: 0, right: 12), size: CGSize(width: 80, height: 38))
     }
     
     required init?(coder: NSCoder) {

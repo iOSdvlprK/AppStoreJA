46ebbf1c874f4f975db95794ae0a9740bebc781c
iOS developer K <iOSdvlprK@gmail.com>
2023-05-29 23:32:59 +0900

    pagination user interface

---
+++ AppStoreJA/Views/Music/MusicLoadingFooter.swift
@@ -0,0 +1,33 @@
+//
+//  MusicLoadingFooter.swift
+//  AppStoreJA
+//
+//  Created by joe on 2023/05/29.
+//
+
+import UIKit
+
+class MusicLoadingFooter: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let aiv = UIActivityIndicatorView(style: .large)
+        aiv.color = .darkGray
+        aiv.startAnimating()
+        
+        let label = UILabel(text: "Loading more...", font: .systemFont(ofSize: 16))
+        label.textAlignment = .center
+        
+        let stackView = VerticalStackView(arrangedSubviews: [
+            aiv, label
+        ], spacing: 8)
+        
+        addSubview(stackView)
+        stackView.centerInSuperview(size: CGSize(width: 200, height: 0))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
